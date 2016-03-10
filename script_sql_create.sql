1af1433276a71d8737d05a240e7496488464f0ca
Thainmpd01229 <thainmpd01229@fpt.edu.vn>
2016-03-10 08:07:00 +0700

    Thainmpd01229-test
    test

---
+++ script_sql_create.sql
@@ -0,0 +1,55 @@
+-- Created by Vertabelo (http://vertabelo.com)
+-- Last modification date: 2016-03-10 01:06:33.051
+
+
+
+
+-- tables
+-- Table: TNhanvien
+CREATE TABLE TNhanvien (
+    MaNV int  NOT NULL,
+    TenNV nvarchar(100)  NOT NULL,
+    Ngaysinh date  NOT NULL,
+    Gioitinh text  NOT NULL,
+    Email text  NOT NULL,
+    Mucluong int  NOT NULL,
+    Maphong nchar(5)  NOT NULL,
+    TPhongban_Maphong int  NOT NULL,
+    CONSTRAINT TNhanvien_pk PRIMARY KEY  (MaNV)
+)
+;
+
+
+
+
+
+-- Table: TPhongban
+CREATE TABLE TPhongban (
+    Tenphong nvarchar(100)  NOT NULL,
+    Maphong nchar(5)  NOT NULL,
+    CONSTRAINT TPhongban_pk PRIMARY KEY  (Maphong)
+)
+;
+
+
+
+
+
+
+
+
+
+-- foreign keys
+-- Reference:  TNhanvien_TPhongban (table: TNhanvien)
+
+ALTER TABLE TNhanvien ADD CONSTRAINT TNhanvien_TPhongban 
+    FOREIGN KEY (Maphong)
+    REFERENCES TPhongban (Maphong)
+;
+
+
+
+
+
+-- End of file.
+
